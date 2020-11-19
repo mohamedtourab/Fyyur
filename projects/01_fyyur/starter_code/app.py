fe7cf2e6795faae8fa4038e1e7ef0b6037ddc26e
mohamedtourab <mohamedmtourab@gmail.com>
2020-11-19 11:12:12 +0100

    Updating models

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -1,7 +1,6 @@
 # ----------------------------------------------------------------------------#
 # Imports
 # ----------------------------------------------------------------------------#
-
 import json
 import dateutil.parser
 import babel
@@ -30,53 +29,50 @@ migrate = Migrate(app, db)
 # ----------------------------------------------------------------------------#
 # Models.
 # ----------------------------------------------------------------------------#
+# venue = Venue(name="The Musical Hop",genres="Jazz,Reggae,Swing,Classical,Folk",address="1015 Folsom Street",city="San Francisco",state="CA",phone="123-123-1234",website="https://www.themusicalhop.com",facebook_link= "https://www.facebook.com/TheMusicalHop",seeking_talent= True,seeking_description="We are on the lookout for a local artist to play every two weeks. Please call us.",image_link= "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60")
+# artist = Artist(name="Guns N Petals",genres="Rock n Roll",city="San Francisco",state="CA",phone="326-123-5000",website="https://www.gunsnpetalsband.com",facebook_link= "https://www.facebook.com/GunsNPetals",seeking_venue= True,seeking_description= "Looking for shows to perform at in the San Francisco Bay Area!",image_link= "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80")
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
-
-# {
-#
-#         "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-#         "website": "https://www.themusicalhop.com",
-#         "seeking_talent": True,
-#         "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-#         "past_shows": [{
-#             "artist_id": 4,
-#             "artist_name": "Guns N Petals",
-#             "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-#             "start_time": "2019-05-21T21:30:00.000Z"
-#         }],
-#         "upcoming_shows": [],
-#         "past_shows_count": 1,
-#         "upcoming_shows_count": 0,
-#     }
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String(), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-
+    website = db.Column(db.String(120))
+    genres = db.Column(db.String())
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String())
+    shows = db.relationship('Show', backref='venue', lazy=True)
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String())
+    shows = db.relationship('Show', backref='artist', lazy=True)
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
 
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
@@ -315,7 +311,7 @@ def show_artist(artist_id):
         "name": "Guns N Petals",
         "genres": ["Rock n Roll"],
         "city": "San Francisco",
-        "state": "CA",
+        "state": "s",
         "phone": "326-123-5000",
         "website": "https://www.gunsnpetalsband.com",
         "facebook_link": "https://www.facebook.com/GunsNPetals",
